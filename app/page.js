49d12ab2c954eef798ef022bde718cda47943441
Rohitrky2021 <102401490+Rohitrky2021@users.noreply.github.com>
2024-08-24 19:59:56 +0530

    UI-frontend

--- app/page.js
+++ app/page.js
@@ -1,14 +1,14 @@
+import Image from "next/image";
 import Component from "@/components/components";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-gray-900 flex items-center justify-center">
-      <div className="container mx-auto p-8 max-w-lg">
-        <h1 className="text-4xl font-extrabold text-white mb-8 text-center">
-          Financial Probe Model
-        </h1>
-        <Component />
-      </div>
+    <div className="min-h-screen bg-gradient-to-r from-blue-200 via-purple-200 to-pink-200 flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-extrabold text-center text-gray-800 mt-12 mb-12">
+        Financial Probe Model
+      </h1>
+
+      <Component />
     </div>
   );
 }
